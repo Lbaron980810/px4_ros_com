8f3fe9fc26d867fc35b2ce85ae51036c0ff89709
lbaron980810 <lbaron980810@163.com>
2022-05-23 19:44:05 +0800

    moveit + offboard with only position

--- src/examples/offboard/offboard_control_origin.cpp
+++ src/examples/offboard/offboard_control_origin.cpp
@@ -1,12 +1,53 @@
+/****************************************************************************
+ *
+ * Copyright 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @brief Offboard control example
+ * @file offboard_control.cpp
+ * @addtogroup examples
+ * @author Mickey Cowden <info@cowden.tech>
+ * @author Nuno Marques <nuno.marques@dronesolutions.io>
+
+ * The TrajectorySetpoint message and the OFFBOARD mode in general are under an ongoing update.
+ * Please refer to PR: https://github.com/PX4/PX4-Autopilot/pull/16739 for more info. 
+ * As per PR: https://github.com/PX4/PX4-Autopilot/pull/17094, the format
+ * of the TrajectorySetpoint message shall change.
+ */
+
 #include <px4_msgs/msg/offboard_control_mode.hpp>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/timesync.hpp>
 #include <px4_msgs/msg/vehicle_command.hpp>
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
-#include <px4_msgs/msg/vehicle_local_position.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <trajectory_msgs/msg/multi_dof_joint_trajectory.hpp>
-#include <trajectory_msgs/msg/multi_dof_joint_trajectory_point.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <stdint.h>
 #include <string>
@@ -41,28 +82,13 @@ public:
 #endif
 
 		// get common timestamp
-		traj_sub_ = 
-			this->create_subscription<trajectory_msgs::msg::MultiDOFJointTrajectory>("/omnihex_traj", 1,
-							[this](const trajectory_msgs::msg::MultiDOFJointTrajectory::SharedPtr msg)
-															   {
-																   trajectoryPtr = msg;
-																   traj_updated = true;
-															   });
-		pos_subscription_ = 
-			this->create_subscription<px4_msgs::msg::VehicleLocalPosition>("VehicleLocalPosition_PubSubTopic", 1, 
-							[this](const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg)
-															   {
-																	current_position_.x = msg->x;
-																	current_position_.y = -msg->y;
-																	current_position_.z = -msg->z;
-															   });
-		offboard_setpoint_counter_ = 0;
 		timesync_sub_ =
 			this->create_subscription<px4_msgs::msg::Timesync>("Timesync_PubSubTopic", 10,
 															   [this](const px4_msgs::msg::Timesync::UniquePtr msg)
 															   {
 																   timestamp_.store(msg->timestamp);
 															   });
+
 		offboard_setpoint_counter_ = 0;
 
 		auto timer_callback = [this]() -> void
@@ -75,10 +101,11 @@ public:
 				// Arm the vehicle
 				this->arm();
 			}
+
 			// offboard_control_mode needs to be paired with trajectory_setpoint
 			publish_offboard_control_mode();
-			publish_trajectory_setpoint(1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
-			
+			publish_trajectory_setpoint();
+
 			// stop the counter after reaching 11
 			if (offboard_setpoint_counter_ < 11)
 			{
@@ -98,22 +125,15 @@ private:
 	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
 	rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr timesync_sub_;
-	rclcpp::Subscription<trajectory_msgs::msg::MultiDOFJointTrajectory>::SharedPtr traj_sub_;
 
 	std::atomic<uint64_t> timestamp_; //!< common synced timestamped
+	uint64_t t0_{};
+	bool t0_set_{false};
 
 	uint64_t offboard_setpoint_counter_; //!< counter for the number of setpoints sent
 
-public:
-
-	float threshold = 1.0;
-	px4_msgs::msg::VehicleLocalPosition current_position_;
-	trajectory_msgs::msg::MultiDOFJointTrajectory::SharedPtr trajectoryPtr;
-	bool traj_updated = false;
-
 	void publish_offboard_control_mode() const;
-	void publish_trajectory_setpoint(float x, float y, float z, 
-						float roll = 0.0, float pitch = 0.0, float yaw = 0.0);
+	void publish_trajectory_setpoint();
 	void publish_vehicle_command(uint16_t command, float param1 = 0.0,
 								 float param2 = 0.0) const;
 };
@@ -149,7 +169,7 @@ void OffboardControl::publish_offboard_control_mode() const
 	msg.position = true;
 	msg.velocity = false;
 	msg.acceleration = false;
-	msg.attitude = true;
+	msg.attitude = false;
 	msg.body_rate = false;
 
 	offboard_control_mode_publisher_->publish(msg);
@@ -160,26 +180,82 @@ void OffboardControl::publish_offboard_control_mode() const
  *        For this example, it sends a trajectory setpoint to make the
  *        vehicle hover at 5 meters with a yaw angle of 180 degrees.
  */
-void OffboardControl::publish_trajectory_setpoint(float x, float y, float z, 
-						float roll, float pitch, float yaw)
+void OffboardControl::publish_trajectory_setpoint()
 {
+	if(offboard_setpoint_counter_ == 11 && (!t0_set_))
+	{
+		t0_ = timestamp_.load();
+		t0_set_ = true;
+	}
+	
 	TrajectorySetpoint msg{};
-	// msg.timestamp = timestamp_.load();
-	msg.x = x;
-	msg.y = -y;
-	msg.z = -z;
+	uint64_t t = timestamp_.load();
 
-	// msg.x = target_pose_.transforms[0].translation.x;
-	// msg.y = -target_pose_.transforms[0].translation.y;
-	// msg.z = -target_pose_.transforms[0].translation.z;
-	msg.roll = roll; // target_pose_.x;
-	msg.pitch = pitch; // target_pose_.x;
-	msg.yaw = yaw; // target_pose_.x;
+    // circle trajectory
+//	float theta = 0.3 * 1e-6 * (t - t0_);
+//	msg.timestamp = t;
+//	msg.x = 0 + 4 * sin(theta);
+//	msg.y = 4 - 4 * cos(theta);
+//	msg.z = -2.5 + sin(theta);
+//	msg.roll = 0 + sin(theta);
+//	msg.pitch = 0 + sin(theta);
+//	msg.yaw = M_PI_2 + theta; // [-PI:PI]
 
-	trajectory_setpoint_publisher_->publish(msg);
+    // 8 shape trajectory
+    double T = 40, t_1 = T / (3 * M_PI + 4), t_2 = 3 * M_PI * T / (6 * M_PI + 8);
+    double r = 1.5, h = 2.5, d_h = 0.5;
+    double t_ = 1e-6 * (t - t0_);
+    while(t_ > T)
+        t_ -= T;
+    msg.z = -h - d_h + d_h * cos(2 * M_PI / (2 * t_1 + t_2) * t_);
+    msg.pitch = d_h * 2 * M_PI / (2 * t_1 + t_2) * sin(2 * M_PI / (2 * t_1 + t_2) * t_);
+    if(t_ >= 0 && t_ < t_1)
+    {
+        msg.x = 0;
+        msg.y = r / t_1 * t_;
+        msg.roll = 0;
+        msg.yaw = M_PI_2;
+    }
+    if(t_ >= t_1 && t_ < t_1 + t_2)
+    {
+        double omega = 3 * M_PI / (2 * t_2);
+        msg.x = -r + r * cos(omega * (t_ - t_1));
+        msg.y = r + r * sin(omega * (t_ - t_1));
+        msg.roll = M_PI / 4 - M_PI / 4 * cos(2 * M_PI / t_2 * (t_ - t_1));
+        msg.yaw = M_PI / 2 + omega * (t_ - t_1);
+    }
+    if(t_ >= t_1 + t_2 && t_ < 3 * t_1 + t_2)
+    {
+        msg.x = -r + r / t_1 * (t_ - t_1 - t_2);
+        msg.y = 0;
+        msg.roll = 0;
+        msg.yaw = 0;
+    }
+    if(t_ >= 3 * t_1 + t_2 && t_ < T - t_1)
+    {
+        double omega = 3 * M_PI / (2 * t_2);
+        msg.x = r + r * sin(omega * (t_ - 3 * t_1 - t_2));
+        msg.y = -r + r * cos(omega * (t_ - 3 * t_1 - t_2));
+        msg.roll = -M_PI / 4 + M_PI / 4 * cos(2 * M_PI / t_2 * (t_ - 3 * t_1 - t_2));
+        msg.yaw = -omega * (t_ - 3 * t_1 - t_2);
+    }
+    if(t_ >= T - t_1 && t_ < T)
+    {
+        msg.x = 0;
+        msg.y = -r + r / t_1 * (t_ - (T - t_1));
+        msg.roll = 0;
+        msg.yaw = M_PI_2;
+    }
+
+
+	while(msg.yaw > M_PI)
+		msg.yaw -= M_PI * 2;
+	while(msg.yaw < -M_PI)
+		msg.yaw += M_PI * 2;
+
+    if(t0_set_)
+	    trajectory_setpoint_publisher_->publish(msg);
 }
-
-
 
 /**
  * @brief Publish vehicle commands
@@ -209,35 +285,7 @@ int main(int argc, char *argv[])
 	std::cout << "Starting offboard control node..." << std::endl;
 	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 	rclcpp::init(argc, argv);
-	auto node = std::make_shared<OffboardControl>();
-	while (rclcpp::ok()) {
-		if (node->traj_updated) {
-			for (auto point : node->trajectoryPtr->points) {
-				std::cout << "current position: " << node->current_position_.x << " "  
-							<< node->current_position_.y << " " << node->current_position_.z << std::endl;
-				std::cout << "target position: " << point.transforms[0].translation.x << " "  
-						<< point.transforms[0].translation.y << " " << point.transforms[0].translation.z << std::endl;
-				float delta_x = node->current_position_.x - point.transforms[0].translation.x;
-				float delta_y = node->current_position_.y - point.transforms[0].translation.y;
-				float delta_z = node->current_position_.z - point.transforms[0].translation.z;
-				float delta = delta_x * delta_x + delta_y * delta_y + delta_z * delta_z;
-				while (delta > node->threshold) {
-					delta_x = node->current_position_.x - point.transforms[0].translation.x;
-					delta_y = node->current_position_.y - point.transforms[0].translation.y;
-					delta_z = node->current_position_.z - point.transforms[0].translation.z;
-					delta = delta_x * delta_x + delta_y * delta_y + delta_z * delta_z;
-					// std::cout << "delta: " << delta << std::endl;
-					node->publish_offboard_control_mode();
-					node->publish_trajectory_setpoint(point.transforms[0].translation.x, point.transforms[0].translation.y, point.transforms[0].translation.z);
-					rclcpp::spin_some(node);
-					// rclcpp::sleep_for(std::chrono::seconds(1));
-				}
-			}
-			node->traj_updated = false;
-			std::cout << "Path Finished" << std::endl;
-		}
-		rclcpp::spin_some(node);
-	}
+	rclcpp::spin(std::make_shared<OffboardControl>());
 
 	rclcpp::shutdown();
 	return 0;
